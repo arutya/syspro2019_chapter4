ae615878c6ba2ebdd20295165cd56cda157e8f3a
arutya <3j02igarashi@gmail.com>
2019-09-26 03:28:42 +0100

    角度を指定してサーボを動作させる関数の追加

--- servo.py
+++ servo.py
@@ -1,25 +1,25 @@
 import time
 import RPi.GPIO as GPIO
 
+
+### function to operate servo by specifying an angle
+def setservo(angle):
+    angle = (0.5 + (1.9 / 180) * angle) / 20 * 100
+    servo.ChangeDutyCycle(angle)
+    time.sleep(1)
+
 GPIO.setmode(GPIO.BCM)
 GPIO.setup(2, GPIO.OUT)
 servo = GPIO.PWM(2, 50)
 servo.start(0.0)
 
-bottom = 2.5
-middle = 7.2
-top = 12.0
+### case : user input data
+print("speciflying the angle")
+angle = input()
 
-for i in range(5):
-	servo.ChangeDutyCycle(bottom)
-	time.sleep(1.0)
+for i in range(3):
+    servo.ChangeDutyCycle(2.5)
+    time.sleep(1)
+    setservo(angle)
 
-	servo.ChangeDutyCycle(middle)
-	time.sleep(1.0)
-
-	servo.ChangeDutyCycle(top)
-	time.sleep(1.0)
-
-
-
-
+GPIO.cleanup()
